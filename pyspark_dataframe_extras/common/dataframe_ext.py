3c90f96bae2dfac9ed57c29f2209c45e9d55209f
Kent Czechowski <kentcz@hotmail.com>
2022-05-17 23:04:24 -0500

    Add function documentation (#8)

--- pyspark_dataframe_extras/common/dataframe_ext.py
+++ pyspark_dataframe_extras/common/dataframe_ext.py
@@ -7,6 +7,13 @@ from pyspark.sql.functions import col, row_number, count, format_number, desc, a
 
 
 def print_count(self, label: Optional[str] = None):
+    """
+    Print the datafame count without terminating a method chain
+
+    :param self: The dataframe
+    :param label: Optional label to prefix the print statement
+    :return: The dataframe
+    """
 
     if label:
         print(f"{label}: {self.count():,}")
@@ -17,6 +24,14 @@ def print_count(self, label: Optional[str] = None):
 
 
 def show_csv(self, n: int = 20, delimiter: str = ","):
+    """
+    An alternative to .show() which prints the table as a CSV, instead of an ASCII table
+
+    :param self: The dataframe
+    :param n: Number of rows to include
+    :param delimiter: Column delimiter for the CSV
+    :return: The dataframe
+    """
     rows = self.limit(n).collect()
 
     fields = [col for col, col_dtype in self.dtypes]
@@ -34,6 +49,13 @@ def show_csv(self, n: int = 20, delimiter: str = ","):
 
 
 def split_by_column(self, col):
+    """
+    Splits the dataframe into multiple dataframes, based on the values in column `col`
+
+    :param self: The dataframe
+    :param col: The column to split on
+    :return: The dataframe
+    """
     value_df = self.groupBy(col).count()
 
     values = [
@@ -48,32 +70,43 @@ def split_by_column(self, col):
     }
 
 
-def select_duplicates(self, group_by_columns, order_by_columns, sentinel_column="duplicate_row_number"):
-    return self\
-        .withColumn(
-            sentinel_column,
-            row_number().over(
-                Window.partitionBy(group_by_columns).orderBy(order_by_columns)
-            )
-        )\
-        .filter(col(sentinel_column) == 1)\
-        .drop(sentinel_column)
-
-
 def frequency_count(self, group_by_column, count_column="count", frequency_column="frequency"):
+    """
+    Group by `group_by_column` then generate `count` and `frequency` columns for the grouped data
+
+    :param self: The dataframe
+    :param group_by_column: The column to group by
+    :param count_column: The desired name for the `count` column
+    :param frequency_column: The desired name for the `frequency` column
+    :return: The dataframe
+    """
     df_count = self.count()
 
-    return self \
-        .groupBy(group_by_column) \
+    return (
+        self
+        .groupBy(group_by_column)
         .agg(
             count("*").alias(count_column)
-        ) \
-        .withColumn(frequency_column, col(count_column) / df_count) \
+        )
+        .withColumn(frequency_column, col(count_column) / df_count)
         .orderBy(desc(count_column))
+    )
 
 
 def show_frequency_count(self, group_by_column, count_column="count", frequency_column="frequency", limit=50,
                          truncate=False, pretty=True):
+    """
+    Show the .frequencyCount() of the dataframe, using a pretty numerical values
+
+    :param self: The dataframe
+    :param group_by_column: The column to group by
+    :param count_column: The desired name of the `count` column
+    :param frequency_column: The desired name of the `frequency` column
+    :param limit: The number of rows
+    :param truncate: Truncate long columns
+    :param pretty: Pretty print the numerical values
+    :return: The dataframe
+    """
     frequency_df = frequency_count(
         self=self,
         group_by_column=group_by_column,
@@ -82,16 +115,26 @@ def show_frequency_count(self, group_by_column, count_column="count", frequency_
     )
 
     if pretty:
-        cleaned_df = frequency_df \
-            .withColumn(count_column, format_number(col(count_column), 0)) \
+        cleaned_df = (
+            frequency_df
+            .withColumn(count_column, format_number(col(count_column), 0))
             .withColumn(frequency_column, format_number(col(frequency_column), 2))
+        )
     else:
         cleaned_df = frequency_df
 
     cleaned_df.show(limit, truncate)
 
 
-def group_by_conditional(self, boolean_exp, group_by_column):
+def conditional_group_by(self, boolean_exp, group_by_column):
+    """
+    Conditionally apply a Group By
+
+    :param self: The dataframe
+    :param boolean_exp: The conditional expression
+    :param group_by_column: The column to group by
+    :return: The dataframe
+    """
     if boolean_exp:
         return self.groupBy(group_by_column)
     else:
@@ -102,12 +145,25 @@ def percentile_agg(
         self, column, group_by_column=None, count_column="count", average_column="average", additional_aggs=[],
         percentile_prefix="_p", percentiles=[0.0, 0.25, 0.50, 0.75, 1.0]
 ):
+    """
+    Calculates the percentiles of an aggregation
+
+    :param self: The dataframe
+    :param column: The primary column
+    :param group_by_column: The column to group by
+    :param count_column: The name of the `count` output column
+    :param average_column: The name of the `average` output column
+    :param additional_aggs: List of additional aggregation columns
+    :param percentile_prefix: String prefix for the name of the output percentile columns
+    :param percentiles: List of percentiles
+    :return: The dataframe
+    """
     percentile_columns = [
         expr(f"percentile({column}, {percentile})").alias(f"{column}{percentile_prefix}{int(percentile * 100)}")
         for percentile in percentiles
     ]
 
-    return group_by_conditional(self, group_by_column is not None, group_by_column) \
+    return conditional_group_by(self, group_by_column is not None, group_by_column) \
         .agg(
         *[
             count("*").alias(count_column),
@@ -118,7 +174,7 @@ def percentile_agg(
     )
 
 
-def is_numeric_dtype(dtype):
+def _is_numeric_dtype(dtype):
     numeric_dtypes = [
         "short",
         "int",
@@ -131,14 +187,36 @@ def is_numeric_dtype(dtype):
 
 
 def numeric_fields(self):
+    """
+    Returns a list of the numeric columns in the dataframe
+    :param self: The dataframe
+    :return: A list of column names
+    """
+    numeric_dtypes = [
+        "short",
+        "int",
+        "bigint",
+        "float",
+        "double",
+        "decimal"
+    ]
+
     return [
         field
         for field, dtype in self.dtypes
-        if is_numeric_dtype(dtype)
+        if dtype in numeric_dtypes
     ]
 
 
 def format_fields(self, exclude_columns=[], d=0):
+    """
+    Formats the numeric columns for displaying
+
+    :param self: The dataframe
+    :param exclude_columns: List of columns to exclude
+    :param d: The number of decimal places to round to
+    :return: The dataframe
+    """
     result_df = self
     for field in numeric_fields(self):
         if field not in exclude_columns:
@@ -147,7 +225,15 @@ def format_fields(self, exclude_columns=[], d=0):
 
 
 def first_in_group(df, group_by, order_by):
-    return df\
+    """
+    Remove duplicates rows from the `group_by` group, retaining the first occurrence based on `order_by` order
+    :param df: The dataframe
+    :param group_by: The columns to group by
+    :param order_by: The columns to order by
+    :return: The dataframe
+    """
+    return (
+        df
         .withColumn(
             "group_row_number",
             row_number().over(
@@ -155,16 +241,17 @@ def first_in_group(df, group_by, order_by):
                 .partitionBy(group_by)
                 .orderBy(order_by)
             )
-        )\
-        .filter(col("group_row_number") == 1)\
+        )
+        .filter(col("group_row_number") == 1)
         .drop("group_row_number")
+    )
 
 
 def pretty_count(self):
     return f"{self.count():,}"
 
 
-DataFrame.groupByConditional = group_by_conditional  # type: ignore[attr-defined]
+DataFrame.groupByConditional = conditional_group_by  # type: ignore[attr-defined]
 DataFrame.numericFields = numeric_fields             # type: ignore[attr-defined]
 DataFrame.formatFields = format_fields               # type: ignore[attr-defined]
 DataFrame.frequencyCount = frequency_count           # type: ignore[attr-defined]
